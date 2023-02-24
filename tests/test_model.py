e4907184d7f2b180fdc7b495aec1b2deddd35d4b
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-02-24 17:00:28 +0100

    Add test for subset_normal

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
-from src.utils.utils import make_labels_binary
+from src.utils.utils import make_labels_binary, subset_normal
 
 @pytest.fixture
 def example_labels():
@@ -19,14 +19,13 @@ def example_le(example_labels):
     return le, int_labels
 
 @pytest.fixture
-def example_df():
-    return (pd.DataFrame({'Feature1': [1, 3, 5, float('Nan'), 100000, 4, -10, 4, 5, float('inf'), float('-inf'), float('nan')],
-                         'Feature2': [3, 12, 5, 3, 3, 1, 4, 10, 51, 5, 10000, -1053445],
-                         'Label': ['BENIGN', 'BENIGN', 'BOT', 'DDoS', 'Trojan', 'Worm', 'Scan', 'BENIGN', 'BENIGN', 'Trojan', 'Worm', 'Scan']}),
-            pd.DataFrame({'Feature1': [1, 3, 5, float('Nan'), 100000, 4, -10, 4, 5, float('inf'), float('-inf'), float('nan')],
-                         'Feature2': [3, 12, 5, 3, 3, 1, 4, 10, 51, 5, 10000, -1053445],
-                         'Label': ['BENIGN', 'BENIGN', 'BOT', 'DDoS', 'Trojan', 'Worm', 'Scan', 'BENIGN', 'BENIGN', 'Trojan', 'Worm', 'Scan']}))
+def example_df_labels():
+    return pd.DataFrame({'Label': [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1]})
 
+@pytest.fixture
+def example_df():
+    return pd.DataFrame({'Feature1': [1, 3, 5, float('Nan'), 100000, 4, -10, 4, 5, float('inf'), float('-inf'), float('nan')],
+                         'Feature2': [3, 12, 5, 3, 3, 1, 4, 10, 51, 5, 10000, -1053445]})
 
 def test_make_labels_binary(example_le):
     le, int_labels = example_le
@@ -36,7 +35,10 @@ def test_make_labels_binary(example_le):
     assert len(unique) == len([0,1])
     assert all([a == b for a, b in zip(unique, [0,1])])
 
-def test_subset_normal(example_df):
-    (x_train, x_test) = example_df
-    x_train, x_test = subset_normal(x_train, x_test)
+def test_subset_normal(example_df, example_df_labels):
+    x_train = example_df
+    y_train = example_df_labels
+    x_train, y_train = subset_normal(x_train, y_train)
+    assert len(x_train) == 5
+    assert len(y_train) == 5
     
