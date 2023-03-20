fb892c68c4e4ff8908c706c4465c668a0b6268fc
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-03-20 21:57:52 +0100

    Change hyperopt for both models

--- src/hyperopt.py
+++ src/hyperopt.py
@@ -3,20 +3,27 @@ import json
 import keras_tuner
 
 from utils.wasserstein import HyperWGAN
+from utils.gan import HyperGAN
 
-def hyperopt(train, val, num_trials):
-
-    with open('config.json', 'r', encoding='utf-8') as f:
-        config = json.loads(f.read())
-
+def hyperopt(model, config, train, val, num_trials):
     num_features = train.x.shape[1]
-    tuner = keras_tuner.BayesianOptimization(
-        hypermodel=HyperWGAN(num_features, config, discriminator_extra_steps=5, gp_weight=10.0),
-        max_trials=num_trials,
-        overwrite=True,
-        directory="./hyperopt",
-        project_name="HyperWGAN",
-    )
+
+    if model == 'wgan':
+        tuner = keras_tuner.BayesianOptimization(
+            hypermodel=HyperWGAN(num_features, config, discriminator_extra_steps=5, gp_weight=10.0),
+            max_trials=num_trials,
+            overwrite=True,
+            directory="./hyperopt",
+            project_name="HyperWGAN",
+        )
+    else:
+        tuner = keras_tuner.BayesianOptimization(
+            hypermodel=HyperGAN(num_features, config),
+            max_trials=num_trials,
+            overwrite=True,
+            directory="./hyperopt",
+            project_name="HyperGAN",
+        )
 
     tuner.search(
         train,
@@ -26,5 +33,4 @@ def hyperopt(train, val, num_trials):
     tuner.results_summary()
 
     return tuner.get_best_hyperparameters(5)[0]
-
 
