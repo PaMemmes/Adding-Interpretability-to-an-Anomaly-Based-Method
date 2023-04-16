43539e8cddd20b13878a8709eb6f8c30e6130cdc
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-04-16 20:06:30 +0200

    Move computation to class

--- suricata_analysis/analysis_comparison.py
+++ suricata_analysis/analysis_comparison.py
@@ -29,13 +29,14 @@ class AllDataHolder():
         self.original = DataHolder('original')
         self.frag = DataHolder('fragmented')
         self.frag_random = DataHolder('fragmented_random')
-        self.original.preprocess_data()
-        self.frag.preprocess_data()
-        self.frag_random.preprocess_data()
+        
+        self.original.process_data()
+        self.frag.process_data()
+        self.frag_random.process_data()
 
     def make_comp_plots(self):
-        plot_comparison_severity_distribution(self.original.total_severity, self.frag.total_severity, save='plots/severity_dist_both.pdf')
-        plot_comparison_packet_alerts(self.original.total_packets, self.frag.total_packets, self.original.total_signatures, self.frag.total_signatures, save='plots/packet_alerts_both.pdf')
+        plot_comparison_packet_alerts(self.original.packets_sum, self.frag.packets_sum, self.original.sigs_sum, self.frag.sigs_sum, save='plots/packet_alerts_both.pdf')
+        plot_comparison_severity_distribution(self.original.sev_dist, self.frag.sev_dist, save='plots/severity_dist_both.pdf')
 
     def make_individual_plots(self):
         self.original.make_plots()
@@ -45,36 +46,63 @@ class AllDataHolder():
 class DataHolder():
     def __init__(self, kind):
         self.kind = kind
-        self.total_packets = []
-        self.total_signatures = []
-        self.total_severity = []
-        self.total_categories = []
-    
-    def preprocess_data(self):
+
+        self.packets = []
+        self.signatures = []
+        self.severity = []
+        self.categories = []
+
+        self.sigs_dist = defaultdict(int)
+        self.sev_dist = defaultdict(int)
+
+        self.sigs_sum = 0
+        self.packets_sum = 0
+
+    def process_data(self):
         for filepath in sorted(glob.glob('theZoo_' + self.kind + '/**/*.json')):
             json_file = get_json_data(filepath)
             alerts, packets = get_alerts_and_packets(json_file)
             nmbr_signatures, severity= get_signatures(alerts)
             cats_frag = get_categories(alerts)
 
-            self.total_packets.append(packets)
-            self.total_signatures.append(nmbr_signatures)
-            self.total_severity.append(severity)
-            self.total_categories.append(cats_frag)
+            self.packets.append(packets)
+            self.signatures.append(nmbr_signatures)
+            self.severity.append(severity)
+            self.categories.append(cats_frag)
 
+        for elem in self.signatures:
+            for value in elem.values():
+                self.sigs_sum += value
+
+        for elem in self.packets:
+            self.packets_sum += elem
+        
+        for signatures in self.signatures:
+            for key, value in signatures.items():
+                self.sigs_dist[key] += value
+        
+        for signatures in self.severity:
+            for key, value in signatures.items():
+                self.sev_dist[value] += 1
+        
+        # Make value 0 for every key that is not there
+        for key in np.arange(len(self.sev_dist)):
+            if key not in self.sev_dist.keys():
+                self.sev_dist[key] = 0
+    
     def plot_file(self, file):
-        file = 'theZoo_original/' + file + '/eve.json'
-        json_data = get_json_data(file)
+        filepath = 'theZoo_original/' + file + '/eve.json'
+        json_data = get_json_data(filepath)
         alerts, _ = get_alerts_and_packets(json_data)
         nmbr_signatures, _ = get_signatures(alerts)
 
-        plot_alerts(nmbr_signatures, file, save=f'plots/' + file + '_' + self.kind + '_original.pdf')
+        plot_alerts(nmbr_signatures, file, save=f'plots/' + file + '_' + self.kind + '.pdf')
 
     def make_plots(self):
-        plot_alert_distribution(self.total_signatures, save='plots/alert_dist_' + self.kind + '.pdf')
-        plot_severity_distribution(self.total_severity, save='plots/severity_dist_' + self.kind + '.pdf')
-        plot_packet_alerts(self.total_packets, self.total_signatures, save='plots/packet_alerts_' + self.kind + '.pdf')
-        plot_categories(self.total_categories, save='plots/cat_dist_' + self.kind + '.pdf')
+        plot_alert_distribution(self.sigs_dist, save='plots/alert_dist_' + self.kind + '.pdf')
+        plot_severity_distribution(self.severity, save='plots/severity_dist_' + self.kind + '.pdf')
+        plot_packet_alerts(self.packets_sum, self.sigs_sum, save='plots/packet_alerts_' + self.kind + '.pdf')
+        plot_categories(self.categories, save='plots/cat_dist_' + self.kind + '.pdf')
         self.plot_file('All.ElectroRAT')
 
 if __name__ == '__main__':
--- suricata_analysis/plots.py
+++ suricata_analysis/plots.py
@@ -22,24 +22,6 @@ def plot_comparison_severity_distribution(dist1, dist2, save=None):
     width = 0.33
 
     severity_range = np.arange(4)
-    severity_range2 = np.arange(4)
-    total_sigs_dist1 = defaultdict(int)
-    for signatures in dist1:
-        for key, value in signatures.items():
-            total_sigs_dist1[value] += 1
-
-    for key in severity_range:
-        if key not in total_sigs_dist1.keys():
-            total_sigs_dist1[key] = 0
-
-    total_sigs_dist2 = defaultdict(int)
-    for signatures in dist2:
-        for key, value in signatures.items():
-            total_sigs_dist2[value] += 1
-
-    for key in severity_range:
-        if key not in total_sigs_dist2.keys():
-            total_sigs_dist2[key] = 0
 
     fig, ax = plt.subplots()
     fig.set_size_inches(6,4)
@@ -54,8 +36,8 @@ def plot_comparison_severity_distribution(dist1, dist2, save=None):
     ax.set_ylabel('Number of Alerts')
     ax.set_xticks(severity_range + width / 2)
     ax.set_xticklabels(('1', '2', '3', '4'))
-    rects1 = ax.bar(severity_range, total_sigs_dist1.values(), width=width, align='center')
-    rects2 = ax.bar(severity_range2+width, total_sigs_dist2.values(), width=width, align='center')
+    rects1 = ax.bar(severity_range, dist1.values(), width=width, align='center')
+    rects2 = ax.bar(severity_range+width, dist2.values(), width=width, align='center')
     ax.bar_label(rects1)
     ax.bar_label(rects2)
     ax.legend((rects1[0], rects2[0]), ('Normal', 'Fragmented'))
@@ -63,26 +45,9 @@ def plot_comparison_severity_distribution(dist1, dist2, save=None):
     if save is not None:
         fig.savefig(save)
     plt.close('all')
-def plot_comparison_packet_alerts(packets, packets_fragmented, total_signatures, total_signatures_fragmented, save=None):   
-    width = 0.33
 
-    total_sigs = 0
-    for elem in total_signatures:
-        for value in elem.values():
-            total_sigs += value
-    
-    total_packets = 0
-    for elem in packets:
-        total_packets += elem
-    
-    total_sigs_fragmented = 0
-    for elem in total_signatures_fragmented:
-        for value in elem.values():
-            total_sigs_fragmented += value
-    
-    total_packets_fragmented = 0
-    for elem in packets_fragmented:
-        total_packets_fragmented += elem
+def plot_comparison_packet_alerts(packets_sum, frag_packets_sum, sigs_sum, frag_sigs_sum, save=None):   
+    width = 0.33
     
     fig, ax = plt.subplots()
     fig.set_size_inches(6,4)
@@ -99,8 +64,8 @@ def plot_comparison_packet_alerts(packets, packets_fragmented, total_signatures,
     ax.set_xticks(np.arange(2) + width / 2)
     ax.set_xticklabels(('Total Alerts', 'Total Packets'))
 
-    bar = ax.bar(np.arange(2), [total_sigs, total_packets], width=width)
-    bar_fragmented = ax.bar(np.arange(2)+width, [total_sigs_fragmented, total_packets_fragmented], width=width)
+    bar = ax.bar(np.arange(2), [sigs_sum, packets_sum], width=width)
+    bar_fragmented = ax.bar(np.arange(2)+width, [frag_sigs_sum, frag_packets_sum], width=width)
 
     ax.legend((bar[0], bar_fragmented[0]), ('Normal', 'Fragmented'))
     ax.bar_label(bar)
@@ -110,17 +75,10 @@ def plot_comparison_packet_alerts(packets, packets_fragmented, total_signatures,
     if save is not None:
         fig.savefig(save)
     plt.close('all')
-def plot_packet_alerts(packets, total_signatures, save=None):    
-    total_sigs = 0
-    for elem in total_signatures:
-        for value in elem.values():
-            total_sigs += value
-    
-    total_packets = 0
-    for elem in packets:
-        total_packets += elem
-    print('Total signatures:', total_sigs)
-    print('Total packets:', total_packets)
+
+def plot_packet_alerts(packets_sum, sigs_sum, save=None):    
+    print('Total signatures:', sigs_sum)
+    print('Total packets:', packets_sum)
     fig, ax = plt.subplots()
     fig.set_size_inches(6,4)
     ax.set_axisbelow(True)
@@ -133,7 +91,7 @@ def plot_packet_alerts(packets, total_signatures, save=None):
     ax.set_xlabel('Category')
     ax.set_ylabel('Number')
     ax.set_xticks(range(2),['Total Alerts', 'Total Packets'])
-    bar = ax.bar(np.arange(2), [total_sigs, total_packets])
+    bar = ax.bar(np.arange(2), [sigs_sum, packets_sum])
     ax.bar_label(bar)
     
     if save is not None:
@@ -163,16 +121,12 @@ def plot_alerts(nmbr_signatures, file, save=None):
         fig.savefig(save, bbox_inches="tight")
     plt.close('all')
 
-def plot_alert_distribution(dist, save=None):
-    total_sigs = defaultdict(int)
-    for signatures in dist:
-        for key, value in signatures.items():
-            total_sigs[key] += value
-    xlabels_new = make_xlabels(total_sigs)
+def plot_alert_distribution(sigs_dist, save=None):
+    xlabels_new = make_xlabels(sigs_dist)
 
     fig, ax = plt.subplots()
     fig.set_size_inches(24,8)
-    ax.set_xticks(np.arange(len(total_sigs)), xlabels_new)
+    ax.set_xticks(np.arange(len(sigs_dist)), xlabels_new)
     ax.set_axisbelow(True)
     ax.yaxis.grid(color='white', linestyle='solid')
     ax.xaxis.grid(color='white', linestyle='solid')
@@ -182,7 +136,7 @@ def plot_alert_distribution(dist, save=None):
     ax.set_title('Alert Distribution')
     ax.set_xlabel('Alert')
     ax.set_ylabel('Number of Alerts')
-    ax.bar(np.arange(len(total_sigs)), total_sigs.values())
+    ax.bar(np.arange(len(sigs_dist)), sigs_dist.values())
     
     if save is not None:
         fig.savefig(save, bbox_inches="tight")
