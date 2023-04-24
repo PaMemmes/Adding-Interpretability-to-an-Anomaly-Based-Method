286bbf09012839992a217d8f151c30ca5000ffec
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-04-24 14:29:57 +0200

    Add preprocess class

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,106 +2,128 @@ import pickle
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.model_selection import train_test_split
 import glob
 import os
 from utils.utils import DataSequence
-from utils.utils import remove_infs, make_labels_binary, subset
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from utils.utils import remove_infs, make_labels_binary, subset, scale, encode
 
 BATCH_SIZE = 256
 
-def preprocess_frag():
-    all_files = glob.glob(os.path.join('../data/csv_fragmentedV3', "*.csv"))
-    df = pd.read_csv('../data/cicids2018/Friday-02-03-2018_TrafficForML_CICFlowMeter.csv')
-    df_frag = pd.concat((pd.read_csv(f) for f in all_files), ignore_index=True)
+class DataFrame():
+    def __init__(self):
+        self.df = pd.read_csv('../data/cicids2018/Friday-02-03-2018_TrafficForML_CICFlowMeter.csv')
+        self.df = self.df.drop('Timestamp', axis=1)
+        self.df_cols = self.df.columns
+        print('Normal data size', self.df.shape)
 
-    df_frag = df_frag.drop(['Dst IP', 'Flow ID', 'Src IP', 'Src Port', 'Timestamp'], axis=1)
-    assert len(df_frag.columns), len(df.columns)
+        df_exe = pd.read_csv('../data/csv_fragmentedV3/All.ElectroRAT.pcap_Flow.csv')
+        df_exe = df_exe.drop(['Dst IP', 'Flow ID', 'Src IP', 'Src Port', 'Timestamp'], axis=1)
+        df_exe['Label'] = 'Fragmented Malware'
+        df_exe.loc[1, 'Label'] = 'ANOMALY'
+        df = pd.concat([df_exe, self.df], ignore_index=True)
+        labels = df['Label']
+        self.le = LabelEncoder()
+        self.le.fit(labels)
 
-    df_frag['Label'] = 'Fragmented Malware'
-    df_frag['Dst Port'] = np.random.randint(0, df.max(axis=0)['Dst Port'],size=(len(df_frag)))
+    def preprocess_frag(self):
+        all_files = glob.glob(os.path.join('../data/csv_fragmentedV3', "*.csv"))
+        self.df_frag = pd.concat((pd.read_csv(f) for f in all_files), ignore_index=True)
 
-    print(df_frag.shape)
-    print(df.shape)
-    return df_frag
+        self.df_frag = self.df_frag.drop(['Dst IP', 'Flow ID', 'Src IP', 'Src Port', 'Timestamp'], axis=1)
+        assert len(self.df_frag.columns), len(self.df_cols)
 
-def preprocess(kind='normal', gan_data=None, frag_data=None):
-    #df = pd.read_csv('../data/cicids2017_kaggle/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv')
-    #df = df.rename(columns={' Label': 'label'})
-    # df = pd.read_csv('../data/cicids2017/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv')
-    df = pd.read_csv('../data/cicids2018/Friday-02-03-2018_TrafficForML_CICFlowMeter.csv')
-    df = df.drop('Timestamp', axis=1)
-    df_cols = list(df)
-    
-    if gan_data is not None:
+        self.df_frag['Label'] = 'Fragmented Malware'
+        self.df_frag['Dst Port'] = np.random.randint(0, self.df.max(axis=0)['Dst Port'],size=(len(self.df_frag)))
+
+    def make_frag_main_df(self):
+        df = self.df_frag
+        df, labels = remove_infs(df)
+
+        labels = encode(self.le, labels)
+        x_train, x_test, y_train, y_test = train_test_split(df, labels, test_size=0.2)
+
+        y_train = make_labels_binary(self.le, y_train)
+        y_test = make_labels_binary(self.le, y_test)
+        x_train, x_test = scale(x_train, x_test)
+
+        self.train_sqc = DataSequence(x_train, y_train, batch_size=BATCH_SIZE)
+        self.test_sqc = DataSequence(x_test, y_test, batch_size=BATCH_SIZE)
+
+        return self.train_sqc, self.test_sqc, self.df_cols
+
+    def preprocess_add(self, gan_data):
         x = np.array(gan_data)
         x = np.array(x).reshape((x.shape[0]*x.shape[1]), x.shape[2])
         y = np.array(['ANOMALY' for i in range(len(x))]).reshape(len(x), 1)
         data = np.concatenate((x,y), axis=1)
-        df_add = pd.DataFrame(data, columns=df_cols)
-        labels_add = df_add['Label']
-        x_train_add = df_add.drop('Label',axis=1)
-        le = LabelEncoder()
-        le.fit(labels_add)
-        int_labels_add = le.transform(labels_add)
-        y_train_add = int_labels_add
-
-    before_removal = len(df)
-    df, labels = remove_infs(df)
-
-    print(f'Length before NaN drop: {before_removal}, after NaN drop: {len(df)}\nThe df is now {len(df)/before_removal} of its original size')
-    le = LabelEncoder()
-    le.fit(labels)
-    int_labels = le.transform(labels)
-
-    x_train, x_test, y_train, y_test = train_test_split(df, int_labels, test_size=0.2)
-
-    assert x_train.shape[0] == y_train.shape[0]
-    assert x_test.shape[0] == y_test.shape[0]
-    assert x_train.shape[1] == x_test.shape[1]
-
-    y_train = make_labels_binary(le, y_train)
-    y_test = make_labels_binary(le, y_test)
-
-    # Subsetting only Normal Network packets in training set
-    if kind == 'normal':
-        x_train, y_train = subset(x_train, y_train, 0)
-        print('Using only normal data')
-    elif kind == 'anomaly':
-        x_train, y_train = subset(x_train, y_train, 1)
-        print('Using only anomaly data')
-
-    scaler = MinMaxScaler()
-
-    x_train = scaler.fit_transform(x_train)
-    x_test = scaler.transform(x_test)
-
-    if gan_data is not None:
-        x_train_add = x_train_add.to_numpy()
-        x_train = np.vstack((x_train, x_train_add))
-        y_train = np.concatenate((y_train, y_train_add))
+        self.df_add = pd.DataFrame(data, columns=self.df_cols)
     
+    def merge_df_add(self):
+        self.df = pd.concat([self.df, self.df_add], ignore_index=True)
     
-    train_sqc = DataSequence(x_train, y_train, batch_size=BATCH_SIZE)
-    test_sqc = DataSequence(x_test, y_test, batch_size=BATCH_SIZE)
-    print('Train data has shape', train_sqc.x.shape, train_sqc.y.shape)
-    print('Test data has shape', test_sqc.x.shape, test_sqc.y.shape)
-    dataset = {}
-    dataset['train'] = train_sqc
-    dataset['test'] = test_sqc
+    def merge_df_frag(self):
+        self.df = pd.concat([self.df, self.df_frag], ignore_index=True)
 
-    preprocessed_data = {
-        'dataset': dataset,
-        'cols': df_cols
-    }
+    def merge_df_all(self):
+        self.df = pd.concat([self.df, self.df_add], ignore_index=True)
+        self.df = pd.concat([self.df, self.df_frag], ignore_index=True)
 
-    with open('../data/preprocessed_data.pickle', 'wb') as file:
-        pickle.dump(preprocessed_data, file)
+    def prepare(self, kind=None):
+        df, labels = remove_infs(self.df)
+
+        #le = LabelEncoder()
+        #le.fit(labels)
+        labels = encode(self.le, labels)
+
+        x_train, x_test, y_train, y_test = train_test_split(df, labels, test_size=0.2)
+
+        y_train = make_labels_binary(self.le, y_train)
+        y_test = make_labels_binary(self.le, y_test)
+
+        # Subsetting only Normal Network packets in training set
+        if kind == 'normal':
+            x_train, y_train = subset(x_train, y_train, 0)
+        elif kind == 'anomaly':
+            x_train, y_train = subset(x_train, y_train, 1)
+            print('Using only anomaly data')
+
+        x_train, x_test = scale(x_train, x_test)
+
+        self.train_sqc = DataSequence(x_train, y_train, batch_size=BATCH_SIZE)
+        self.test_sqc = DataSequence(x_test, y_test, batch_size=BATCH_SIZE)
+
+        return self.train_sqc, self.test_sqc, self.df_cols
+
+def get_frags():
+    data = DataFrame()
+    data.preprocess_frag()
+    train, test, df_cols = data.make_frag_main_df()
+    return train, test, df_cols
+
+def preprocess(kind=None, additional=False, frag_data=False):
+    data = DataFrame()
     
-    return train_sqc
-
-if __name__ == '__main__':
-    pd.set_option('display.max_columns', 500)
-    preprocess_frag()
-    preprocess()
+    if kind is None and frag_data == False and additional is None:
+        train, test, df_cols = data.prepare()
+        return train, test, df_cols
+    elif kind is None and frag_data == True and additional is not None:
+        data.preprocess_add(additional)
+        data.preprocess_frag()
+        data.merge_df_all()
+        train, test, df_cols = data.prepare()
+        return train, test, df_cols
+    elif kind is None and frag_data == True and additional is None:
+        data.preprocess_frag()
+        data.merge_df_frag()
+        train, test, df_cols = data.prepare()
+        return train, test, df_cols
+    elif kind is None and frag_data == False and additional is not None:
+        data.preprocess_add(additional)
+        data.merge_df_add()
+        train, test, df_cols = data.prepare()
+        return train, test, df_cols
+    else:
+        train, test, df_cols = data.prepare(kind)
+        return train, test, df_cols
