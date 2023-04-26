83063cb2d283949127a5aa683e312469a8cfd0b1
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-04-26 12:52:08 +0200

    Fix tests

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -7,7 +7,7 @@ from src.utils.utils import remove_infs
 def example_df():
     return pd.DataFrame({'Feature1': [1, 3, 5, float('Nan'), 100000, 4, -10, 4, 5, float('inf'), float('-inf'), float('nan')],
                          'Feature2': [3, 12, 5, 3, 3, 1, 4, 10, 51, 5, 10000, -1053445],
-                         'label': ['BENIGN', 'BENIGN', 'BOT', 'DDoS', 'Trojan', 'Worm', 'Scan', 'BENIGN', 'BENIGN', 'Trojan', 'Worm', 'Scan']})
+                         'Label': ['BENIGN', 'BENIGN', 'BOT', 'DDoS', 'Trojan', 'Worm', 'Scan', 'BENIGN', 'BENIGN', 'Trojan', 'Worm', 'Scan']})
 
 
 def test_remove_infs(example_df):
