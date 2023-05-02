02fda1105b0626131f429676c8c26d1aca4e143d
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-05-02 17:57:36 +0200

    Minor adjustments

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 import collections
 from math import floor
 BATCH_SIZE = 256
+FILENAME = 'Friday-02-03-2018_TrafficForML_CICFlowMeter.csv'
 # python3 main.py combined Y 1 1 1
 
 if __name__ =='__main__':
@@ -21,15 +22,15 @@ if __name__ =='__main__':
     data = DataFrame()
     if args.file == 'xg':
         if args.frag_data == 'Y':
-            data.preprocess(kind=None, frags=True)
+            data.preprocess_anomalies_only_frags(filename=FILENAME, kind=None, frags=True)
             model = xg_main(train=data.train_sqc, test=data.test_sqc, frags=data.test_frag_sqc, trials=args.trials, save='train_w_frags_xg')
-            interpret_tree(model, data.test_sqc, data.df_cols, save='train_w_frags_xg')
+            interpret_tree(model, data, save='train_w_frags_xg')
         else:
-            data.preprocess()
+            data.preprocess(filename=FILENAME)
             model = xg_main(train=data.train_sqc, test=data.test_sqc, frags=data.test_frag_sqc, trials=args.trials, save='train_wo_frags_xg')
-            interpret_tree(model, data.test_sqc, data.df_cols, save='train_wo_frags_xg')
+            interpret_tree(model, data, save='train_wo_frags_xg')
     elif args.file == 'combined':
-        data.preprocess(kind=None, frags=False)
+        data.preprocess_anomalies_only_frags(filename=FILENAME,kind=None, frags=False)
         model = train('wgan', data.train_sqc, data.test_sqc, None, args.trials, args.retraining, args.epochs, save='combined_wgan')
         gan_data = []
         for i in range(floor(len(data.train_sqc.x) / BATCH_SIZE / 10)):
@@ -38,19 +39,19 @@ if __name__ =='__main__':
             gan_data.append(fake_x)
 
         if args.frag_data =='Y':
-            data.preprocess(kind=None, frags=True, add=gan_data)
+            data.preprocess_anomalies_only_frags(filename=FILENAME, kind=None, frags=True, add=gan_data)
             model = xg_main(train=data.train_sqc, test=data.test_sqc, frags=data.test_frag_sqc, trials=args.trials, save='train_w_frags_combined')
-            interpret_tree(model, data.test_sqc, data.df_cols, save='train_w_frags_combined')
+            interpret_tree(model, data, save='train_w_frags_combined')
         else:
-            data.preprocess(kind=None, frags=False, add=gan_data)
+            data.preprocess_anomalies_only_frags(filename=FILENAME, kind=None, frags=False, add=gan_data)
             model = xg_main(train=data.train_sqc, test=data.test_sqc, frags=data.test_frag_sqc, trials=args.trials, save='train_wo_frags_combined')
-            interpret_tree(model, data.test_sqc, data.df_cols, save='train_wo_frags_combined')
-        
+            interpret_tree(model, data, save='train_wo_frags_combined')
+
     elif args.file == 'wgan' or args.file == 'gan':
         if args.frag_data =='Y':
-            data.preprocess(kind='normal', frags=True, add=None)
-            train(args.file, data.train_sqc, data.test_sqc, data.test_frag_sqc, args.trials, args.retraining, args.epochs, save='train_w_frags_' + args.file)
+            data.preprocess(filename=FILENAME, kind='normal', frags=True, add=None)
+            train(args.file, data, args.trials, args.retraining, args.epochs, save='train_w_frags_' + args.file)
         else:
-            data.preprocess(kind='normal',frags=False, add=None)
-            train(args.file, data.train_sqc, data.test_sqc, data.test_frag_sqc, args.trials, args.retraining, args.epochs, save='train_wo_frags_' + args.file)
+            data.preprocess(filename=FILENAME, kind='normal',frags=False, add=None)
+            train(args.file, data, args.trials, args.retraining, args.epochs, save='train_wo_frags_' + args.file)
 
