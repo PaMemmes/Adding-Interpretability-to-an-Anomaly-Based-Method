e8d867d8c5dc079a4c099c3d165e9f16b5341456
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-05-15 21:04:01 +0200

    Change names of models

--- src/main.py
+++ src/main.py
@@ -6,18 +6,18 @@ from interpretable import interpret_tree
 import tensorflow as tf
 import collections
 from math import floor
+
 BATCH_SIZE = 256
-# python3 main.py combined Y 1 1 1
 
 def run_xg(trials, frags=True):
     if frags == True:
-        name = '_frags'
+        name = 'xg_frags'
     else:
-        name = ''
+        name = 'xg'
     data = DataFrame()
     data.preprocess(filename=None, kind=None, frags=name)
-    model = xg_main(train=data.train_sqc, test=data.test_sqc, frags=data.test_frag_sqc, trials=trials, save='train_xg' + name)
-    interpret_tree(model, data, save='train_xg' + name)
+    model = xg_main(train=data.train_sqc, test=data.test_sqc, frags=data.test_frag_sqc, trials=trials, save=name)
+    interpret_tree(model, data, save=name)
 
 def run_nn(model_name, epochs, retrain, trials, frags=True):
     if frags == True:
@@ -26,7 +26,7 @@ def run_nn(model_name, epochs, retrain, trials, frags=True):
         name = ''
     data = DataFrame()
     data.preprocess(filename=None, kind='normal', frags=frags, add=None)
-    train(model_name=model_name, data=data, frags=data.test_frag_sqc, trials=trials, num_retraining=retrain, epochs=args.epochs, save='train_' + model_name + '_' + name)
+    train(model_name=model_name, data=data, frags=data.test_frag_sqc, trials=trials, num_retraining=retrain, epochs=args.epochs, save=model_name + '_' + name)
 
 def run_combined(epochs, retrain, trials, frags=True):
     if frags == True:
@@ -44,8 +44,8 @@ def run_combined(epochs, retrain, trials, frags=True):
     print('Additional data: ', len(gan_data) * BATCH_SIZE)
     data = DataFrame()
     data.preprocess(filename=None, kind=None, frags=frags, add=gan_data)
-    model = xg_main(train=data.train_sqc, test=data.test_sqc, frags=data.test_frag_sqc, trials=args.trials, save='train_combined' + name)
-    interpret_tree(model, data, save='train_combined' + name)
+    model = xg_main(train=data.train_sqc, test=data.test_sqc, frags=data.test_frag_sqc, trials=args.trials, save='combined' + name)
+    interpret_tree(model, data, save='combined' + name)
 
 if __name__ =='__main__':    
     
@@ -55,14 +55,15 @@ if __name__ =='__main__':
     parser.add_argument('epochs', help='Number of epochs to train: [0, inf]', type=int)
     args = parser.parse_args()
 
+    run_nn('wgan', args.epochs, args.retraining, args.trials, frags=True)
+    run_nn('wgan', args.epochs, args.retraining, args.trials, frags=False)
+
     run_xg(args.trials, frags=False)
     run_xg(args.trials, frags=True)
 
-    run_nn('WGAN', args.epochs, args.retraining, args.trials, frags=True)
-    run_nn('WGAN', args.epochs, args.retraining, args.trials, frags=False)
 
-    run_nn('GAN', args.epochs, args.retraining, args.trials, frags=False)
-    run_nn('GAN', args.epochs, args.retraining, args.trials, frags=True)
+    run_nn('gan', args.epochs, args.retraining, args.trials, frags=False)
+    run_nn('gan', args.epochs, args.retraining, args.trials, frags=True)
 
 
     run_combined(args.epochs, args.retraining, args.trials, frags=False)
