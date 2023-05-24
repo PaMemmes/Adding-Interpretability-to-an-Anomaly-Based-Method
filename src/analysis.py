b1a3a6b12cab8b4c4156d18b0bae3d12f7397cee
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-05-24 18:26:28 +0200

    Replace with proper plot of actual data

--- src/analysis.py
+++ src/analysis.py
@@ -6,17 +6,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def bar_plot():
-    y = [83.070, 7.786, 4.031, 2.347, 1.763, 0.997, 0.006]
-    x = ['Benign', 'DDoS', 'DoS', 'Brute Force', 'Botnet', 'Infiltration', 'Web Attack']
-    data = pd.DataFrame({"Percentage": y, "Attack Type": x})
+# def bar_plot_old():
+#     y = [83.070, 7.786, 4.031, 2.347, 1.763, 0.997, 0.006]
+#     x = ['Benign', 'DDoS', 'DoS', 'Brute Force', 'Botnet', 'Infiltration', 'Web Attack']
+#     data = pd.DataFrame({"Percentage": y, "Attack Type": x})
+#     sns.set_style("whitegrid", {'axes.grid' : True})
+#     ax = sns.barplot(x=data['Attack Type'], y = data['Percentage'])
+#     ax.set_xticklabels(ax.get_xticklabels(), fontsize=10)
+#     plt.tight_layout()
+#     ax.bar_label(ax.containers[0], fmt='%.3f')
+#     fig = ax.get_figure()
+#     fig.savefig("../distribution_cicids2018.pdf", bbox_inches="tight")
+#     plt.close()
+
+def bar_plot_real(df):
+    df = pd.read_csv('../data/cicids2018/Friday-02-03-2018_TrafficForML_CICFlowMeter.csv')
     sns.set_style("whitegrid", {'axes.grid' : True})
-    ax = sns.barplot(x=data['Attack Type'], y = data['Percentage'])
-    ax.set_xticklabels(ax.get_xticklabels(), fontsize=10)
+    ax = sns.histplot(data=df, x='Label', stat='probability')
     plt.tight_layout()
-    ax.bar_label(ax.containers[0], fmt='%.3f')
+    ax.set(ylabel='Percent')
     fig = ax.get_figure()
     fig.savefig("../distribution_cicids2018.pdf", bbox_inches="tight")
+    plt.close()
 
 
 if __name__ == '__main__':
@@ -26,4 +37,4 @@ if __name__ == '__main__':
     print(df.head())
     print(len(df.columns))
 
-    bar_plot()
+    bar_plot_real(df)
