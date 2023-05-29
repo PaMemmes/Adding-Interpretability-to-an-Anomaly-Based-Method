73bfc4f5a5ba7ec14eacb09c5bc799d93c2d88f5
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-05-29 18:22:13 +0200

    Add dependence plots and use only 1000 shapley

--- src/interpretable.py
+++ src/interpretable.py
@@ -4,6 +4,7 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+from pathlib import Path
 
 import pandas as pd
 from collections import defaultdict
@@ -15,8 +16,8 @@ from utils.utils import read_csv, NumpyEncoder
 
 def make_interpret_plots(explainer, shap_values, test_x, df_cols, name):
     if len(shap_values) > 1000:
-        shap_values = shap_values[:1000, :]
-        test_x = test_x[:1000, :]
+        shap_values = shap_values[:1000]
+        test_x = test_x[:1000]
     f = shap.force_plot(
         explainer.expected_value,
         shap_values,
@@ -41,12 +42,19 @@ def make_interpret_plots(explainer, shap_values, test_x, df_cols, name):
     f.savefig(name + 'summary.pdf', bbox_inches='tight', dpi=300)
     plt.close()
 
+    for col in df_cols:
+        print('Column', col)
+        shap.dependence_plot(col, shap_values, test_x, show=False)
+        f = plt.gcf()
+        f.savefig(name + col.replace('/', '_')  + '_dependence.pdf', bbox_inches='tight', dpi=300)
+        plt.close()
+
     shapleys = {
         'Expected_value': explainer.expected_value,
         'Shapleys': shap_values,
-        'text_x': test_x
-
+        'text_x': test_x.to_numpy()
     }
+
     dumped = json.dumps(shapleys, cls=NumpyEncoder)
     with open(name + 'shapley.json', 'w', encoding='utf-8') as f:
         json.dump(dumped, f)
@@ -64,7 +72,7 @@ def interpret_tree(model, data, save):
     make_interpret_plots(
         explainer,
         shap_values,
-        data.test_sqc.x,
+        test_df,
         df_cols,
         name)
 
@@ -74,16 +82,16 @@ def interpret_tree(model, data, save):
     make_interpret_plots(
         explainer,
         shap_values,
-        data.test_frag_sqc.x,
+        test_df,
         df_cols,
         name_frags)
 
-    data.seperate_dfs()
+    data.seperate_dfs(filename=None)
 
     for label, test_sqc in data.seperate_tests.items():
-        name = '../experiments/' + save + '/best/' + label + '_'
-
+        name = '../experiments/' + save + '/best/' + label + '/'
+        Path(name).mkdir(parents=True, exist_ok=True)
         df = pd.DataFrame(test_sqc.x, columns=df_cols)
         explainer = shap.TreeExplainer(model)
         shap_values = explainer.shap_values(df)
-        make_interpret_plots(explainer, shap_values, test_sqc.x, df_cols, name)
+        make_interpret_plots(explainer, shap_values, test_df, df_cols, name)
