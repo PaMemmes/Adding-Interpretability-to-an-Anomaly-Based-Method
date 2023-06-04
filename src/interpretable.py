762e23c07f8af5ba2a76d12254dc12b9f0157817
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-06-05 00:21:17 +0200

    Add feature values + make plots individuals

--- src/interpretable.py
+++ src/interpretable.py
@@ -14,16 +14,96 @@ from preprocess import DataFrame
 
 from utils.utils import read_csv, NumpyEncoder
 
+def make_local_plots(explainer, shap_values, test_x, preds, test_y, df_cols, name):
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    for j in range(len(test_x)):
+        if preds[j] == 1 and preds[j] == test_y[j]:
+            if a == 15:
+                break
+            shap.force_plot(
+                explainer.expected_value,
+                shap_values[j, :],
+                test_x.iloc[j, :].values,
+                feature_names=df_cols.values,
+                matplotlib=True,
+                show=False)
+            plt.savefig(name + 'force_plot_' + str(a) + '_pred(1)_truth(1).pdf', bbox_inches='tight', dpi=300)
+            plt.close()
+            shap.decision_plot(explainer.expected_value, shap_values[j, :], test_x.iloc[j,:], feature_names=df_cols.values,
+                link='logit', highlight=0, show=False)
+            plt.savefig(name + 'decision_plot_' + str(a) + '_pred(1)_truth(1).pdf', bbox_inches='tight', dpi=300)
+            plt.close()
+            a+=1
+        
+        elif preds[j] == 0 and preds[j] == test_y[j]:
+            if b == 15: 
+                break
+            shap.force_plot(
+                explainer.expected_value,
+                shap_values[j, :],
+                test_x.iloc[j, :].values,
+                feature_names=df_cols.values,
+                matplotlib=True,
+                show=False)
+            plt.savefig(name + 'force_plot_' + str(b) + '_pred(0)_truth(0).pdf', bbox_inches='tight', dpi=300)
+            plt.close()
+            shap.decision_plot(explainer.expected_value, shap_values[j, :], test_x.iloc[j,:], feature_names=df_cols.values,
+                link='logit', highlight=0, show=False)
+            plt.savefig(name + 'decision_plot_' + str(b) + '_pred(0)_truth(0).pdf', bbox_inches='tight', dpi=300)
+            plt.close()
+            d+=1
 
-def make_interpret_plots(explainer, shap_values, test_x, test_y, df_cols, name):
+        elif preds[j] == 1 and preds[j] != test_y[j]:
+            if c == 15:
+                break
+            shap.force_plot(
+                explainer.expected_value,
+                shap_values[j, :],
+                test_x.iloc[j, :].values,
+                feature_names=df_cols.values,
+                matplotlib=True,
+                show=False)
+            plt.savefig(name + 'force_plot_' + str(c) + '_pred(1)_truth(0).pdf', bbox_inches='tight', dpi=300)
+            plt.close()
+            shap.decision_plot(explainer.expected_value, shap_values[j, :], test_x.iloc[j,:], feature_names=df_cols.values,
+                link='logit', highlight=0, show=False)
+            plt.savefig(name + 'decision_plot_' + str(c) + '_pred(1)_truth(0).pdf', bbox_inches='tight', dpi=300)
+            plt.close()
+            c+=1
+
+        elif preds[j] == 0 and preds[j] != test_y[j]:
+            if d == 15:
+                break
+            shap.force_plot(
+                explainer.expected_value,
+                shap_values[j, :],
+                test_x.iloc[j, :].values,
+                feature_names=df_cols.values,
+                matplotlib=True,
+                show=False)
+            plt.savefig(name + 'force_plot_' + str(d) + '_pred(0)_truth(1).pdf', bbox_inches='tight', dpi=300)
+            plt.close()
+            shap.decision_plot(explainer.expected_value, shap_values[j, :], test_x.iloc[j,:], feature_names=df_cols.values,
+                link='logit', highlight=0, show=False)
+            plt.savefig(name + 'decision_plot_' + str(d) + '_pred(0)_truth(1).pdf', bbox_inches='tight', dpi=300)
+            plt.close()
+            d += 1
+        if a == 15 and b == 15 and c == 15 and d == 15:
+            break
+            
+
+def make_interpret_plots(explainer, shap_values, test_x, preds, test_y, df_cols, name):
     # Makes plots for interpretation
     # Creates for the first 15 instances force and decision plots
     # Creates summary and summary bar plots for the first 20 (default) features
     # Creates summary and summary bar plots for all features
     # Creates a dependence plot for each feature, dependence is choosing automatically by SHAP
-    if len(shap_values) > 1000:
-        shap_values = shap_values[:1000]
-        test_x = test_x[:1000]
+    if len(shap_values) > 2000:
+        shap_values = shap_values[:2000]
+        test_x = test_x[:2000]
     f = shap.force_plot(
         explainer.expected_value,
         shap_values,
@@ -33,36 +113,23 @@ def make_interpret_plots(explainer, shap_values, test_x, test_y, df_cols, name):
         show=False)
     shap.save_html(name + 'force_plot.htm', f)
     plt.close()
-    for i in range(15):
-        shap.force_plot(
-            explainer.expected_value,
-            shap_values[i, :],
-            test_x.iloc[i, :].values,
-            feature_names=df_cols.values,
-            matplotlib=True,
-            show=False)
-        plt.savefig(name + 'force_plot' + str(i) + '_' + str(int(test_y[i])) + '.pdf', bbox_inches='tight', dpi=300)
-        plt.close()
-        shap.decision_plot(explainer.expected_value, shap_values[i, :], test_x.iloc[i,:], feature_names=df_cols.values,
-            link='logit', highlight=0, show=False)
-        plt.savefig(name + 'decision_plot' + str(i) + '_' + str(int(test_y[i])) + '_.pdf', bbox_inches='tight', dpi=300)
-        plt.close()        
 
+    feature_names = [a + ": " + str(b) for a,b in zip(df_cols, np.abs(shap_values).mean(0).round(2))]
     shap.summary_plot(
         shap_values,
         test_x,
         plot_type="bar",
-        feature_names=df_cols,
+        feature_names=feature_names,
         show=False)
     f = plt.gcf()
     f.savefig(name + 'summary_bar.pdf', bbox_inches='tight', dpi=300)
     plt.close()
-    
+
     shap.summary_plot(
             shap_values,
             test_x,
             plot_type="bar",
-            feature_names=df_cols,
+            feature_names=feature_names,
             max_display=len(df_cols),
             show=False
             )
@@ -70,16 +137,17 @@ def make_interpret_plots(explainer, shap_values, test_x, test_y, df_cols, name):
     f.savefig(name + 'summary_bar_all.pdf', bbox_inches='tight', dpi=300)
     plt.close()
 
-
-    shap.summary_plot(shap_values, test_x, feature_names=df_cols,show=False)
+    shap.summary_plot(shap_values, test_x, feature_names=feature_names, show=False)
     f = plt.gcf()
     f.savefig(name + 'summary.pdf', bbox_inches='tight', dpi=300)
     plt.close()
 
-    shap.summary_plot(shap_values, test_x, feature_names=df_cols, max_display=len(df_cols), show=False)
+    shap.summary_plot(shap_values, test_x, feature_names=feature_names, max_display=len(df_cols), show=False)
     f = plt.gcf()
     f.savefig(name + 'summary_all.pdf', bbox_inches='tight', dpi=300)
     plt.close()
+
+    make_local_plots(explainer, shap_values, test_x, preds, test_y, df_cols, name)
 
     for col in df_cols:
         print('Column', col)
@@ -107,8 +175,6 @@ def feature_importance(model, df_cols, importance_type):
     return weight, y
 
 def plot_importance(model, name, df_cols, importance_type):
-    plt.style.use(['ieee', 'science'])
-    plt.style.use('seaborn-colorblind')
     fig, ax = plt.subplots()
     fig.set_size_inches(8,6)
     ax.set_axisbelow(True)
@@ -135,6 +201,7 @@ def interpret_tree(model, data, save):
 
     print('Starting interpreting...')
 
+    preds = model.predict(data.test_sqc.x)
     df_cols = data.df_cols[:-1]
     test_df = pd.DataFrame(data.test_sqc.x, columns=df_cols)
     explainer = shap.TreeExplainer(model)
@@ -143,6 +210,7 @@ def interpret_tree(model, data, save):
         explainer,
         shap_values,
         test_df,
+        preds,
         data.test_sqc.y,
         df_cols,
         name)
@@ -150,6 +218,7 @@ def interpret_tree(model, data, save):
     plot_importance(model, name, df_cols, 'gain')
     plot_importance(model, name, df_cols, 'weight')
 
+    preds = model.predict(data.test_frag_sqc.x)
     test_df = pd.DataFrame(data.test_frag_sqc.x, columns=df_cols)
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(test_df, check_additivity=False)
@@ -157,6 +226,7 @@ def interpret_tree(model, data, save):
         explainer,
         shap_values,
         test_df,
+        preds,
         data.test_sqc.y,
         df_cols,
         name_frags)
@@ -166,7 +236,8 @@ def interpret_tree(model, data, save):
     for label, test_sqc in data.seperate_tests.items():
         name = '../experiments/' + save + '/best/' + label + '/'
         Path(name).mkdir(parents=True, exist_ok=True)
+        preds = model.predict(test_sqc.x)
         df = pd.DataFrame(test_sqc.x, columns=df_cols)
         explainer = shap.TreeExplainer(model)
         shap_values = explainer.shap_values(df, check_additivity=False)
-        make_interpret_plots(explainer, shap_values, df, data.test_sqc.y, df_cols, name)
+        make_interpret_plots(explainer, shap_values, df, preds, data.test_sqc.y, df_cols, name)
